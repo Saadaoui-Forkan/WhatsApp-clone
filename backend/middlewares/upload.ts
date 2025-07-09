d6737990fef577105926ee38b3850a56eda8a9f9
unknown <forkan.saadaoui@gmail.com>
2025-07-09 10:38:57 +0100

    ignore lastes commits

--- backend/middlewares/upload.ts
+++ backend/middlewares/upload.ts
@@ -1,52 +1,33 @@
 import path from "path";
-import multer, { FileFilterCallback } from "multer";
+import multer from "multer";
 import { fileURLToPath } from "url";
-import fs from "fs";
-import { Request } from "express";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const isProduction = process.env.NODE_ENV === "production";
-const uploadDir = isProduction ? "/tmp" : path.join(__dirname, "../images");
-
-if (!isProduction && !fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir, { recursive: true });
-}
-
+// Photo Storage
 const photoStorage = multer.diskStorage({
-  destination: function (
-    req: Request,
-    file: Express.Multer.File,
-    cb: (error: Error | null, destination: string) => void
-  ) {
-    cb(null, uploadDir);
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, "../images"));
   },
-  filename: function (
-    req: Request,
-    file: Express.Multer.File,
-    cb: (error: Error | null, filename: string) => void
-  ) {
+  filename: function (req, file, cb) {
     if (file) {
       cb(null, new Date().toISOString().replace(/:/g, "-") + file.originalname);
     } else {
-      cb(new Error("File upload error"), "");
+      cb(new Error("File upload error"), "")
     }
   },
 });
 
+// Photo Upload Middleware
 export const photoUpload = multer({
   storage: photoStorage,
-  fileFilter: function (
-    req: Request,
-    file: Express.Multer.File,
-    cb: FileFilterCallback
-  ) {
-    if (file.mimetype.startsWith("image")) {
-      cb(null, true);
-    } else {
-      cb(new Error("Unsupported File Format"));
-    }
+  fileFilter: function(req, file, cb) {
+      if (file.mimetype.startsWith("image")) {
+          cb(null, true)
+      } else {
+          cb(new Error("Unsupported File Format"))
+      }
   },
-  limits: { fieldSize: 1024 * 1024 * 5 },
-});
+  limits: { fieldSize: 1024 * 1024 * 5 }
+})
