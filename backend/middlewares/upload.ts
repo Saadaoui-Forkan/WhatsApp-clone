c7dbd1b26df6d9f0e45b29c61b095afff2553fda
unknown <forkan.saadaoui@gmail.com>
2025-07-09 19:17:56 +0100

    second essay

--- backend/middlewares/upload.ts
+++ backend/middlewares/upload.ts
@@ -2,15 +2,12 @@ import path from "path";
 import multer from "multer";
 import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const imagePath = path.resolve(__dirname, "../images");
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 // Photo Storage
 const photoStorage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, imagePath);
-  },
+  destination: "images",
   filename: function (req, file, cb) {
     if (file) {
       cb(null, new Date().toISOString().replace(/:/g, "-") + file.originalname);
